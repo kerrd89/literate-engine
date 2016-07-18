6c1e6302faafcf3ba8cac86b86188747df626153
David Kerr <kerrd89@gmail.com>
2016-07-18 15:12:55 -0600

    storing prior background but attempting to set background to cached RGB giving error that something in my setBackground function is not a function

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+// document.body.style.backgroundColor = 'rgb(' + (localStorage.getItem('backgroundColor')).join(',') + ')';
+
+
+// changeBackground(localStorage.getItem('backgroundColor'));
+
 var randomButton = document.querySelector('.btn');
 var clearButton = document.querySelector('.btn-clear');
 
 
 function generateRandomBackground() {
   var generateRandomNumber = function() {
-    return (Math.floor(Math.random() * (255 - 1 + 1)) + 1);
+    return (Math.floor(Math.random() * (254 - 0 + 0)) + 0);
   }
-  return [generateRandomNumber(),generateRandomNumber(),generateRandomNumber()];
+  return  [generateRandomNumber(),generateRandomNumber(),generateRandomNumber()];
 };
+
+var randomColor = 0;
 
 function resetBackground() {
   return [255,255,255];
 };
 
-function changeBackground(RGBcolor) {
-  document.body.style.backgroundColor = 'rgb(' + RGBcolor.join(',') + ')';
+function changeBackground(randomColor) {
+  document.body.style.backgroundColor = 'rgb(' + randomColor.join(',') + ')';
+  localStorage.setItem('backgroundColor',randomColor);
 };
 
 randomButton.addEventListener('click', function() {
